3c8e914c877d583ec0e582d16a58f84d71da3224
Ian Walter <ianwalter@fastmail.com>
2015-11-10 11:40:09 -0500

    Formatting changes in delete tests

--- test/mix/tasks/bloodhound/delete_test.exs
+++ test/mix/tasks/bloodhound/delete_test.exs
@@ -14,7 +14,7 @@ defmodule Bloodhound.DeleteTest do
   test "all documents can be deleted" do
     Client.index "test", %{id: 1, message: "All Falls Down"}
     Client.index "example", %{id: 2, message: "Can't Tell Me Nothing"}
-    Delete.run([])
+    Delete.run []
     assert {:error, %{status_code: 404}} = Client.get "test", 5
     assert {:error, %{status_code: 404}} = Client.get "example", 2
   end
@@ -22,15 +22,15 @@ defmodule Bloodhound.DeleteTest do
   test "all documents of a type can be deleted" do
     Client.index "test", %{id: 2, message: "All of the Lights"}
     ExampleModel.index %ExampleModel{id: 1, message: "Through the Wire"}
-    Delete.run(["Bloodhound.ExampleModel"])
+    Delete.run ["Bloodhound.ExampleModel"]
     assert {:ok, document} = Client.get "test", 2
     assert document.message === "All of the Lights"
     assert {:error, %{status_code: 404}} = ExampleModel.get_index 1
   end
 
   test "a model's index can be deleted" do
-    ExampleModel.index %ExampleModel{id: 2, message: "Lampshades On Fire"}
-    Delete.run(["Bloodhound.ExampleModel", "2"])
+    ExampleModel.index %ExampleModel{id: 2, message: "Lift Off"}
+    Delete.run ["Bloodhound.ExampleModel", "2"]
     assert {:error, %{status_code: 404}} = ExampleModel.get_index 2
   end
 end
