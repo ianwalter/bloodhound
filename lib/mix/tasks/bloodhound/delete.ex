a2e722ba9503f9c15666bc069d1acfdd26a71979
Ian Walter <ianwalter@fastmail.com>
2015-11-10 13:32:52 -0500

    Adding messages to delete task

--- lib/mix/tasks/bloodhound/delete.ex
+++ lib/mix/tasks/bloodhound/delete.ex
@@ -5,14 +5,20 @@ defmodule Mix.Tasks.Bloodhound do
     alias Bloodhound.Client
 
     def run(options) do
+      {:ok, _} = Application.ensure_all_started(:bloodhound)
+
       case options do
-        [] -> Client.delete
+        [] ->
+          Client.delete
+          IO.puts "Deleted #{Application.get_env :bloodhound, :index} index"
         [module_name] ->
           {module, _} = Code.eval_string(module_name)
           module.delete_index
+          IO.puts "Deleted #{module.index_type} index"
         [module_name, id] ->
           {module, _} = Code.eval_string(module_name)
           module.delete_index id
+          IO.puts "Deleted #{module.index_type}, #{id} index"
       end
     end
   end
